b8d1e041966e522c7f74f1535c7e6c3e1dcea5ec
dinhtheanh998 <dinhtheanh198@gmail.com>
2022-05-15 14:31:31 +0700

    edit-fraction

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import icons from 'url:../../img/icons.svg';
 import View from './View.js';
-import { fractional } from 'fractional';
+import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
